f1308f7b7c37895d051295a352d4c26ebee45bbb
jael-dev <jaelngaleu@gmail.com>
2024-05-09 17:51:45 +0200

    Replaced all Nan by 0

--- modules/imports.py
+++ modules/imports.py
@@ -18,7 +18,7 @@ def create_teams_df():
             team_id = data[side]['id']
             team_name = data[side]['club']
             teams_data.append({'idteam': team_id, 'name': team_name})
-    return pd.DataFrame(teams_data).drop_duplicates()
+    return pd.DataFrame(teams_data).fillna(0).drop_duplicates()
 
 def create_players_df():
     players_data=[]
@@ -30,14 +30,14 @@ def create_players_df():
                 player_id = p_id
                 lastname = p_info['info']['lastname']
                 players_data.append({'idplayer': player_id, 'lastname': lastname})
-    return pd.DataFrame(players_data).drop_duplicates()
+    return pd.DataFrame(players_data).fillna(0).drop_duplicates()
 
 def create_formations_types_df(side,data):
     players=data[side]['players']
     for p_id, p_info in players.items():
         formation = p_info['info']['formation_used']
         return formation 
-    return None 
+    return 0 
 
 def create_matches_df():
     matches_data=[]
@@ -49,15 +49,15 @@ def create_matches_df():
         date = data['dateMatch']
         duration = data['matchTime']
         period = data['period']
-        championship = data.get('championship', None)
-        quotation_pre_game = data.get('quotationPreGame', {})
-        quotation_away = quotation_pre_game.get('Away', None)
-        quotation_home = quotation_pre_game.get('Home', None)
-        quotation_draw = quotation_pre_game.get('Draw', None)
+        championship = data.get('championship', 0)
+        quotation_pre_game = data.get('quotationPreGame', 0)
+        quotation_away = quotation_pre_game.get('Away', 0)
+        quotation_home = quotation_pre_game.get('Home', 0)
+        quotation_draw = quotation_pre_game.get('Draw', 0)
         home_formation = create_formations_types_df('Home', data)
         away_formation = create_formations_types_df('Away', data)
-        home_score =  len(data.get('matchData', {}).get('home', {}).get('goals', []))
-        away_score =  len(data.get('matchData', {}).get('away', {}).get('goals', []))
+        home_score =  len(data.get('matchData', 0).get('home', 0).get('goals', 0))
+        away_score =  len(data.get('matchData', 0).get('away', 0).get('goals', 0))
         matches_data.append({
             'matchid': matchid, 'hometeam': home_team['club'], 'awayteam': away_team['club']
             , 'date':date, 'duration':duration,
@@ -68,7 +68,7 @@ def create_matches_df():
             'home_formation': home_formation,
             'away_formation': away_formation
             ,'home_score': home_score, 'away_score': away_score})
-    return pd.DataFrame(matches_data)
+    return pd.DataFrame(matches_data).fillna(0).drop_duplicates()
 
 
 def create_highlight_df():
@@ -79,10 +79,10 @@ def create_highlight_df():
         matchdata = data['matchData']
         for timeline in matchdata['timeline']:
             time = timeline['time']
-            playerid = timeline.get('playerId', None)
+            playerid = timeline.get('playerId', 0)
             thetype = timeline['type']
             highlight_data.append({'matchid': matchid, 'time': time, 'playerid': playerid, 'type': thetype})
-    return pd.DataFrame(highlight_data).drop_duplicates()
+    return pd.DataFrame(highlight_data).fillna(0).drop_duplicates()
 
 
 def create_substitutions_df():
@@ -92,16 +92,18 @@ def create_substitutions_df():
         matchid = data['id']
         for side in ['home', 'away']:
             sside = data['matchData'][side]
-            for substitution in sside.get('substitutions', []):
+            for substitution in sside.get('substitutions', 0):
                 time = substitution['time']
                 player_in = substitution['subOn']
                 player_out = substitution['subOff']
-                reason = substitution.get('reason', None)
+                reason = substitution.get('reason', 0)
                 substitution_data.append({'matchid': matchid, 'time': time, 'on_playerid': player_in, 'off_playerid': player_out, 'reason': reason})
-    return pd.DataFrame(substitution_data).drop_duplicates()
+    return pd.DataFrame(substitution_data).fillna(0).drop_duplicates()
 
 def get_player_quotation(quotation_players, player_id):
     player_key = f"player_{player_id}"
+    if player_key not in quotation_players:
+        return 0
     return quotation_players.get(player_key)
 
 def create_match_players_df():
@@ -119,16 +121,16 @@ def create_match_players_df():
                 position = player_info['position']
                 formation_place = player_info['formation_place']
                 quotation_player = get_player_quotation(data.get('quotationPlayers', {}), player_id)
-                final_mark_2015 = player_info.get('note_final_2015', None)  
+                final_mark_2015 = player_info.get('note_final_2015', 0)  
                 play_duration = player_info['mins_played']
                 stats = player_stat.get('stat', {})
-                player_stats = {f"{k}": v for k, v in stats.items()}
+                player_stats = {f"{k}": (v if pd.notna(v) else 0) for k, v in stats.items()}
                 match_players_data.append({
                     'matchid': matchid, 'teamid': team_id, 'playerid': player_id, 'position': position,
                     'formation_place': formation_place, 'quotation_player': quotation_player,
                     'final_mark_2015': final_mark_2015, 'play_duration': play_duration, **player_stats})
 
-    return pd.DataFrame(match_players_data).drop_duplicates()
+    return pd.DataFrame(match_players_data).fillna(0).drop_duplicates()
 
 def create_historique_transferts_df():
     historique_data = pd.DataFrame(columns=['player_id', 'name', 'team_name', 'start_date', 'end_date'])
